7453a1e4a06bb0d71621d7668e5dfcae5155cbd6
Amarnath0510 <amarmuthu007@gmail.com>
2022-01-14 23:18:11 +0530

    .env added

--- index.js
+++ index.js
@@ -1,86 +1,90 @@
 
 import express, { request, response } from "express";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app=express();
 const PORT=7001;
 app.use(express.json())
-const rooms=[
-    {
-      "id": "100",
-      "customername": "Amarnath",
-      "roomname": "Elite28",
-      "bookedstatus": "confirmed",
-      "date": "22/11/21",
-      "starttime": "10am",
-      "endtime": "8pm"
-    },
-    {
-      "id": "101",
-      "customername": "Suresh",
-      "roomname": "Elite29",
-      "bookedstatus": "confirmed",
-      "date": "22/11/21",
-      "starttime": "6am",
-      "endtime": "7pm"
-    },
-    {
-      "id": "102",
-      "customername": "Murali",
-      "roomname": "Elite30",
-      "bookedstatus": "waiting",
-      "date": "23/11/21",
-      "starttime": "9am",
-      "endtime": "10pm"
-    },
-    {
-      "id": "103",
-      "customername": "Edwin",
-      "roomname": "Elite31",
-      "bookedstatus": "waiting",
-      "date": "23/11/21",
-      "starttime": "5am",
-      "endtime": "8pm"
-    },
-    {
-      "id": "104",
-      "customername": "Steve",
-      "roomname": "Elite32",
-      "bookedstatus": "confirmed",
-      "date": "22/11/21",
-      "starttime": "11am",
-      "endtime": "10pm"
-    },
-    {
-      "id": "105",
-      "customername": "Mohammad",
-      "roomname": "Elite33",
-      "bookedstatus": "confirmed",
-      "date": "22/11/21",
-      "starttime": "2am",
-      "endtime": "1pm"
-    },
-    {
-      "id": "106",
-      "customername": "Priya",
-      "roomname": "Elite34",
-      "bookedstatus": "waiting",
-      "date": "22/11/21",
-      "starttime": "10am",
-      "endtime": "8pm"
-    },
-    {
-      "id": "107",
-      "customername": "Amshika",
-      "roomname": "Elite35",
-      "bookedstatus": "confirmed",
-      "date": "22/11/21",
-      "starttime": "5am",
-      "endtime": "4pm"
-    }
-  ]
+// const rooms=[
+//     {
+//       "id": "100",
+//       "customername": "Amarnath",
+//       "roomname": "Elite28",
+//       "bookedstatus": "confirmed",
+//       "date": "22/11/21",
+//       "starttime": "10am",
+//       "endtime": "8pm"
+//     },
+//     {
+//       "id": "101",
+//       "customername": "Suresh",
+//       "roomname": "Elite29",
+//       "bookedstatus": "confirmed",
+//       "date": "22/11/21",
+//       "starttime": "6am",
+//       "endtime": "7pm"
+//     },
+//     {
+//       "id": "102",
+//       "customername": "Murali",
+//       "roomname": "Elite30",
+//       "bookedstatus": "waiting",
+//       "date": "23/11/21",
+//       "starttime": "9am",
+//       "endtime": "10pm"
+//     },
+//     {
+//       "id": "103",
+//       "customername": "Edwin",
+//       "roomname": "Elite31",
+//       "bookedstatus": "waiting",
+//       "date": "23/11/21",
+//       "starttime": "5am",
+//       "endtime": "8pm"
+//     },
+//     {
+//       "id": "104",
+//       "customername": "Steve",
+//       "roomname": "Elite32",
+//       "bookedstatus": "confirmed",
+//       "date": "22/11/21",
+//       "starttime": "11am",
+//       "endtime": "10pm"
+//     },
+//     {
+//       "id": "105",
+//       "customername": "Mohammad",
+//       "roomname": "Elite33",
+//       "bookedstatus": "confirmed",
+//       "date": "22/11/21",
+//       "starttime": "2am",
+//       "endtime": "1pm"
+//     },
+//     {
+//       "id": "106",
+//       "customername": "Priya",
+//       "roomname": "Elite34",
+//       "bookedstatus": "waiting",
+//       "date": "22/11/21",
+//       "starttime": "10am",
+//       "endtime": "8pm"
+//     },
+//     {
+//       "id": "107",
+//       "customername": "Amshika",
+//       "roomname": "Elite35",
+//       "bookedstatus": "confirmed",
+//       "date": "22/11/21",
+//       "starttime": "5am",
+//       "endtime": "4pm"
+//     }
+//   ]
   
-const MONGO_URL="mongodb://localhost";
+// const MONGO_URL="mongodb://localhost";
+
+const MONGO_URL=process.env.MONGO_URL;
 
  async  function createConnection() {
     const client= new MongoClient(MONGO_URL);
@@ -105,11 +109,7 @@ app.get("/rooms",async(request,response)=>{
 
   
   console.log(request.query);
-  const bookedstatus =await client
-  .db("task41")
-  .collection("rooms")
-  .aggregate([{$match:{bookedstatus:"confirmed"}}])
-  .toArray();
+  const bookedstatus =await getBookedCustomerdata();
   console.log(bookedstatus);
   response.send(bookedstatus);
 });
@@ -119,11 +119,8 @@ app.get("/rooms",async(request,response)=>{
 
   
   console.log(request.query);
-  const bookedcustomer =await client
-  .db("task41")
-  .collection("rooms")
-  .aggregate([{$match:{bookedstatus:"confirmed"}},{ $group: {_id:"$customername"}}])
- .toArray();
+  const bookedcustomer =await getBookedRoomName();
+
   console.log(bookedcustomer);
   response.send(bookedcustomer);
 });
@@ -132,10 +129,7 @@ app.get("/rooms",async(request,response)=>{
 
 app.post("/rooms",async(request,response)=>{
   const data=request.body;
-  const add=await client
-  .db("task41")
-  .collection("rooms")
-  .insertMany(data);
+  const add=await createRoom(data);
   response.send(add);
 
 });
@@ -145,10 +139,7 @@ app.post("/rooms",async(request,response)=>{
 app.get("/rooms/:id",async(request,response)=>{
     console.log(request.params);
     const{id}=request.params;
-    const room= await client
-    .db("task41")
-    .collection("rooms")
-    .findOne({id:id});
+    const room= await getRoomById(id);
   
     console.log(room);
   
@@ -159,3 +150,35 @@ app.get("/rooms/:id",async(request,response)=>{
 });
 
 app.listen(PORT,()=>console.log("App is started in",PORT))
+
+
+
+async function getBookedRoomName() {
+  return await client
+    .db("task41")
+    .collection("rooms")
+    .aggregate([{ $match: { bookedstatus: "confirmed" } }, { $group: { _id: "$customername" } }]);
+}
+
+async function createRoom(data) {
+  return await client
+    .db("task41")
+    .collection("rooms")
+    .insertMany(data);
+}
+
+async function getBookedCustomerdata() {
+  return await client
+    .db("task41")
+    .collection("rooms")
+    .aggregate([{ $match: { bookedstatus: "confirmed" } }])
+    .toArray();
+}
+
+async function getRoomById(id) {
+  return await client
+    .db("task41")
+    .collection("rooms")
+    .findOne({ id: id });
+}
+
